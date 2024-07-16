7ca7ffeda5c648ee4fe094d4ea9434cfc12370d7
ChikhalkarS <Shubhamchikhalkar25@gmail.com>
2024-07-16 15:53:06 +0530

    #Palindrome

---
+++ src/main/java/org/byteclub/com/datastuctureandalgorithms/PalindromeNumber.java
@@ -0,0 +1,21 @@
+package org.byteclub.com.datastuctureandalgorithms;
+
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.print(isPalindrome(1234321));
+    }
+
+    private static boolean isPalindrome(int number) {
+        int reversedNumber =0;
+        int originalNumner = number;
+        while(number !=0)
+        {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        return reversedNumber==originalNumner;
+    }
+}
